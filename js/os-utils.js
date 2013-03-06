f635b826284c9bdf466db16808cd1b52e5a94975
Javier Cerviño <jcague@gmail.com>
2013-03-06 14:35:04 +0100

    I added a SM proxy URL

--- js/os-utils.js
+++ js/os-utils.js
@@ -64,6 +64,9 @@ UTILS.Auth = (function(U, undefined) {
             image.endpoints[0].adminURL = image.endpoints[0].adminURL.replace(/130\.206\.80\.11:9292/, "130.206.80.93/glance");
             image.endpoints[0].publicURL = image.endpoints[0].publicURL.replace(/130\.206\.80\.11:9292/, "130.206.80.93/glance");
             image.endpoints[0].internalURL = image.endpoints[0].internalURL.replace(/130\.206\.80\.11:9292/, "130.206.80.93/glance");
+            compute.endpoints[0].adminURL = compute.endpoints[0].adminURL.replace(/130\.206\.80\.91:8774\/v2\.0\/FIWARE\/vdc/, "130.206.80.93/sm");
+            compute.endpoints[0].publicURL = compute.endpoints[0].publicURL.replace(/130\.206\.80\.91:8774\/v2\.0\/FIWARE\/vdc/, "130.206.80.93/sm");
+            compute.endpoints[0].internalURL = compute.endpoints[0].internalURL.replace(/130\.206\.80\.91:9292\/v2\.0\/FIWARE\/vdc/, "130.206.80.93/sm");
             OVF.API.configure(JSTACK.Keystone.getservice("sm").endpoints[0].publicURL, JSTACK.Keystone.params.access.token.id);
             callback();
         };
