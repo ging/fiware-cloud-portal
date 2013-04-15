14113d9e5b9997cb31d41983a962eda378af0f54
Álvaro Alonso <aalonsog@dit.upm.es>
2013-04-15 18:12:41 +0200

    Added product SDC list

--- js/views/nova/instanceDetail/SDCView.js
+++ js/views/nova/instanceDetail/SDCView.js
@@ -6,9 +6,13 @@ var InstanceSDCView = Backbone.View.extend({
     tableViewNew: undefined,
 
     initialize: function() {
-        this.model.fetch();
-        this.model.unbind("reset");
+        
         this.model.bind("reset", this.render, this);
+        this.model.fetch();
+
+        this.options.instanceModel.bind("change", this.render, this);
+        this.options.instanceModel.fetch();
+
         this.renderFirst();
     },
 
@@ -34,12 +38,16 @@ var InstanceSDCView = Backbone.View.extend({
                 return true;
             }
         };
-        return [];
-        // [{
-        //     label: "Release Floating IPs",
-        //     action: "release",
-        //     activatePattern: groupSelected
-        // }];
+        return [{
+            label: "Uninstall",
+            action: "uninstall",
+            activatePattern: groupSelected
+            },
+            {
+            label: "Edit attributes",
+            action: "edit",
+            activatePattern: oneSelected
+        }];
     },
 
     getHeaders: function() {
@@ -49,19 +57,13 @@ var InstanceSDCView = Backbone.View.extend({
         }, {
             name: "Name",
             tooltip: "Software name",
-            size: "40%",
+            size: "55%",
             hidden_phone: false,
             hidden_tablet: false
         }, {
-            name: "Version",
-            tooltip: "Software version",
-            size: "25%",
-            hidden_phone: true,
-            hidden_tablet: false
-        }, {
-            name: "Available",
-            tooltip: "Software availability",
-            size: "25%",
+            name: "Status",
+            tooltip: "Software state",
+            size: "35%",
             hidden_phone: true,
             hidden_tablet: false
         }];
@@ -69,6 +71,28 @@ var InstanceSDCView = Backbone.View.extend({
 
     getEntries: function() {
         var entries = [];
+
+        if (this.model.models.length === 0) {
+            return entries;
+        }
+
+        var id = this.options.instanceModel.get("id");
+        if (id) {
+
+            var products= this.model.models;
+
+            console.log('PPP', products, id);
+
+            for (var product in products) {
+                var stat = products[product].get('status');
+                if (products[product].get('vm').fqn === id) {// && stat !== 'ERROR' && stat !== 'UNINSTALLED') {
+                    entries.push({id:products[product].get('name'), cells:[
+                    {value: products[product].get('productRelease').product.name},
+                    {value: products[product].get('status')}]});
+                }
+            }
+        }
+
         return entries;
     },
 
@@ -94,12 +118,16 @@ var InstanceSDCView = Backbone.View.extend({
                 return true;
             }
         };
-        return [];
-        // [{
-        //     label: "Release Floating IPs",
-        //     action: "release",
-        //     activatePattern: groupSelected
-        // }];
+        return [{
+            label: "Install",
+            action: "install",
+            activatePattern: groupSelected
+        },
+        {
+            label: "View attributes",
+            action: "view",
+            activatePattern: oneSelected
+        }];
     },
 
     getHeadersNew: function() {
@@ -114,7 +142,7 @@ var InstanceSDCView = Backbone.View.extend({
             hidden_tablet: false
         }, {
             name: "Description",
-            tooltip: "Description",
+            tooltip: "Software description",
             size: "55%",
             hidden_phone: false,
             hidden_tablet: false
@@ -128,10 +156,12 @@ var InstanceSDCView = Backbone.View.extend({
             return entries;
         }
 
-        var products = this.model.models[0].attributes.product;
+        var products = this.model.catalogueList;
+
+        console.log('papapappapa', this.model);
 
         for (var product in products) {
-            entries.push({id:product, cells:[
+            entries.push({id:products[product].name, cells:[
                 {value: products[product].name},
                 {value: products[product].description}]});
         }
@@ -142,30 +172,70 @@ var InstanceSDCView = Backbone.View.extend({
     onClose: function() {
         this.tableView.close();
         this.tableViewNew.close();
+        this.model.unbind("reset");
+        this.options.instanceModel.unbind("change");
         this.unbind();
         this.undelegateEvents();
     },
 
     onAction: function(action, ids) {
-        // var floating, floa, subview;
-        // var self = this;
-        // if (floatingIds.length === 1) {
-        //     floating = floatingIds[0];
-        //     floa = this.model.get(floating);
-        // }
-        // switch (action) {
-        //     case 'allocate':
-        //         subview = new AllocateIPView({el: 'body',  model: this.model});
-        //         subview.render();
-        //     break;
-        //     case 'release':
-        //         subview = new ConfirmView({el: 'body', title: "Release Floating IP", btn_message: "Release Floating IPs", onAccept: function() {
-        //             var subview2 = new MessagesView({el: '#content', state: "Success", title: "Floating IPs "+floa.get(name)+" released."});
-        //             subview2.render();
-        //         }});
-        //         subview.render();
-        //     break;
-        // }
+
+        var ip = this.options.instanceModel.get("addresses").private[0].addr;
+        var fqn = this.options.instanceModel.get("id");
+
+        console.log("vaaaaaaaaaa", action, ids, ip);
+
+        switch (action) {
+            case 'install':
+
+                var product = new SDC();
+
+                product.set({"ip": ip});
+                product.set({"product": {name: ids[0]}});
+                product.set({"fqn": fqn});
+
+                product.save(undefined, {success: function (model, resp) {
+                    console.log('Success Installing ', model, resp);
+                }, error: function (model, e) {
+                    console.log('Error installing ', e);
+                }});
+                break;
+            case 'uninstall':
+
+                //var product = this.model.get(ids[0]);
+                var product = this.model.findWhere({name: ids[0]});
+                console.log('UNISNDT', product);
+                product.destroy({success: function (model, resp) {
+                    console.log('Success uninstalling ', resp);
+                }, error: function (model, e) {
+                    console.log('Error uninstalling ', e);
+                }});
+                break;
+            case 'view':
+
+                this.model.getCatalogueProductDetails({id: ids[0], callback: function (resp) {
+                    subview = new ViewProductAttributesView({el: 'body', productAttributes: resp});
+                    subview.render();  
+                }, error: function (model, e) {
+                    console.log('Error attr ', e);
+                }});
+
+                              
+                break;
+            case 'edit':
+                var product = this.model.findWhere({name: ids[0]});
+                product.fetch({success: function (model, resp) {
+                    var attr = resp.productRelease.product.attributes_asArray;
+                    subview = new EditProductAttributesView({el: 'body', model: this.model, productAttributes: attr});
+                    subview.render();
+                }, error: function (model, e) {
+                    console.log('Error attr ', e);
+                }});
+                //getProductInstance(id)
+                //reconfigureProductInstance(id, att)
+
+                break;
+        }
     },
 
     renderFirst: function() {
@@ -197,7 +267,8 @@ var InstanceSDCView = Backbone.View.extend({
     },
 
     render: function() {
-        if ($(this.el).html() !== null) {
+      var product = this.model;
+            if ($(this.el).html() !== null) {
             this.tableView.render();
             this.tableViewNew.render();
         }
