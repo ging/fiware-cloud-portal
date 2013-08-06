536e2b800566e504a3fe5ebe5842a816d732cb5e
aalonsog <aalonsog@dit.upm.es>
2013-08-06 11:11:36 +0200

    Modified polling interval

--- js/routes/os-routes.js
+++ js/routes/os-routes.js
@@ -25,8 +25,8 @@ var OSRouter = Backbone.Router.extend({
     currentView: undefined,
 
     timers: {},
-    backgroundTime: 60,
-    foregroundTime: 5,
+    backgroundTime: 180,
+    foregroundTime: 10,
 
     routes: {
         'auth/login': 'login',
