1b8b66f6015cb106a93e1901e8730d88a2b17bc8
aalonsog <aalonsog@dit.upm.es>
2014-02-06 19:18:33 +0100

    Fixed a bug in detail view

--- js/views/nova/instanceDetail/InstanceConnectionView.js
+++ js/views/nova/instanceDetail/InstanceConnectionView.js
@@ -13,10 +13,10 @@ var InstanceConnectionView = Backbone.View.extend({
         var self = this;
         this.options = this.options || {};
 
-        this.model.fetch({success: function(){
+        this.model.fetch({success: function(model){
             if (JSTACK.Keystone.getservice("network") !== undefined) {
-                if (self.model.get("addresses") != null) {
-                    var addresses = self.model.get("addresses");
+                if (model.get("addresses") != null) {
+                    var addresses = model.get("addresses");
                     for (var i in addresses) {
                         var ips = addresses[i];
                         for (var j in ips) {
@@ -28,9 +28,9 @@ var InstanceConnectionView = Backbone.View.extend({
                     }
                 }
             } else {
-                if ((self.model.get("addresses") != null) && self.model.get("addresses")["public"] !== null) {
-                    if (self.model.get("addresses")["public"] !== undefined) {
-                        this.public_ip = self.model.get("addresses")["public"][0]; 
+                if ((model.get("addresses") != null) && model.get("addresses")["public"] !== null) {
+                    if (model.get("addresses")["public"] !== undefined) {
+                        this.public_ip = model.get("addresses")["public"][0]; 
                     }
                 }
             }              
