329e2aeadcc890c08343101d89ce563f684bdd38
Javier Cerviño <jcervino@dit.upm.es>
2014-07-24 15:02:17 +0200

    Resolved an error accessing instance in Floating IP view

--- js/views/forms/AssociateIPView.js
+++ js/views/forms/AssociateIPView.js
@@ -32,18 +32,33 @@ var AssociateIPView = Backbone.View.extend({
         var instance_id = this.$("#instance_switcher option:selected").val();
         var instance = this.options.instances.get(instance_id);
         var addr, addresses;
-        if ((instance.get("addresses") != null) && (instance.get("addresses")["public"] !== null || instance.get("addresses")["private"] !== null)) {
-            addresses = instance.get("addresses")["public"];
-            for (var addr_idx in addresses) {
-                addr = addresses[addr_idx].addr;
-                html += '<option value="'+ addr + '">'+addr+'</option>"';
+
+        if (JSTACK.Keystone.getservice("network") !== undefined) {
+            if (instance.get("addresses") != null) {
+                addresses = instance.get("addresses");
+                for (var i in addresses) {
+                    var ips = addresses[i];
+                    for (var j in ips) {
+                        addr = ips[j].addr;
+                        html += '<option value="'+ addr + '">'+addr+'</option>"';
+                    }
+                }
             }
-            addresses = instance.get("addresses")["private"];
-            for (var addr_idx2 in addresses) {
-                addr = addresses[addr_idx2].addr;
-                html += '<option value="'+ addr + '">'+addr+'</option>"';
+        } else {
+            if ((instance.get("addresses") != null) && (instance.get("addresses")["public"] !== null || instance.get("addresses")["private"] !== null)) {
+                addresses = instance.get("addresses")["public"];
+                for (var addr_idx in addresses) {
+                    addr = addresses[addr_idx].addr;
+                    html += '<option value="'+ addr + '">'+addr+'</option>"';
+                }
+                addresses = instance.get("addresses")["private"];
+                for (var addr_idx2 in addresses) {
+                    addr = addresses[addr_idx2].addr;
+                    html += '<option value="'+ addr + '">'+addr+'</option>"';
+                }
             }
         }
+        
         $('#instance_ip_pool').html(html);
     },
 
