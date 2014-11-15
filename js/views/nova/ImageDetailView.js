c5e09ad67091aaf7266642bac37460fe8de3ebb8
Álvaro Alonso <aalonsog@dit.upm.es>
2014-11-15 15:30:14 +0000

    Added launch button in image detail view

--- js/views/nova/ImageDetailView.js
+++ js/views/nova/ImageDetailView.js
@@ -2,9 +2,29 @@ var ConsultImageDetailView = Backbone.View.extend({
 
     _template: _.itemplate($('#consultImageDetailFormTemplate').html()),
 
+    events: {
+        'click #launch_img': 'onLaunch'
+    },
+
     initialize: function() {
+
+        this.options.volumeSnapshotsModel = UTILS.GlobalModels.get("volumeSnapshotsModel");
+        this.options.instancesModel = UTILS.GlobalModels.get("instancesModel");
+        this.options.volumesModel = UTILS.GlobalModels.get("volumesModel");
+        this.options.flavors = UTILS.GlobalModels.get("flavors");
+        this.options.keypairs = UTILS.GlobalModels.get("keypairsModel");
+        this.options.secGroups = UTILS.GlobalModels.get("securityGroupsModel");
+        this.options.quotas = UTILS.GlobalModels.get("quotas");
+        this.options.networks = UTILS.GlobalModels.get("networks");
+        this.options.ports = UTILS.GlobalModels.get("ports");
+
         this.model.bind("change", this.render, this);
         this.model.fetch();
+    },
+
+    onLaunch: function(evt) {
+        var subview = new LaunchImageView({el: 'body', images: this.options.images, flavors: this.options.flavors, keypairs: this.options.keypairs, secGroups: this.options.secGroups, quotas: this.options.quotas, instancesModel: this.options.instancesModel, networks: this.options.networks, tenant: this.options.tenant, volumes: this.options.volumesModel, volumeSnapshots: this.options.volumeSnapshotsModel, ports: this.options.ports, model: this.model});
+        subview.render();
     },
 
     render: function () {
