1e6af62801bb88a8951c447649cb38f4ff722db4
aalonsog <aalonsog@dit.upm.es>
2015-04-29 12:00:58 +0200

    Fixed a bug in monitoring

--- js/models/InstanceModel.js
+++ js/models/InstanceModel.js
@@ -200,22 +200,22 @@ var Instance = Backbone.Model.extend({
                 JSTACK.Nova.getsecuritygroupforserver(model.get("id"), options.success, options.error, this.getRegion());
                 break;
             case "getMonitoringStats":
-                var ip;
-                if (JSTACK.Keystone.getendpoint(UTILS.Auth.getCurrentRegion(), "network") !== undefined) {
-                    if (model.get("addresses") != null) {
-                        var ips = model.get("addresses")[Object.keys(model.get("addresses"))[0]];
-                        for (var p in ips) {
-                            if (ips[p]['OS-EXT-IPS:type'] === 'floating') {
-                                ip = ips[p].addr;
-                            }
-                        }
-                    }
-                } else {
-                    if ((model.get("addresses") != null) && (model.get("addresses")["public"] !== null || model.get("addresses")["private"] !== null)) {
-                        ip = model.get("addresses")["public"][0];
-                    }
-                }
-                Monitoring.API.getVMmeasures(ip, options.success, options.error, this.getRegion());
+                // var ip;
+                // if (JSTACK.Keystone.getendpoint(UTILS.Auth.getCurrentRegion(), "network") !== undefined) {
+                //     if (model.get("addresses") != null) {
+                //         var ips = model.get("addresses")[Object.keys(model.get("addresses"))[0]];
+                //         for (var p in ips) {
+                //             if (ips[p]['OS-EXT-IPS:type'] === 'floating') {
+                //                 ip = ips[p].addr;
+                //             }
+                //         }
+                //     }
+                // } else {
+                //     if ((model.get("addresses") != null) && (model.get("addresses")["public"] !== null || model.get("addresses")["private"] !== null)) {
+                //         ip = model.get("addresses")["public"][0];
+                //     }
+                // }
+                Monitoring.API.getVMmeasures(model.get("id"), options.success, options.error, this.getRegion());
                 break;
         }
     },
