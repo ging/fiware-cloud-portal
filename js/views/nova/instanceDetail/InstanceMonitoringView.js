800483d1e39cdf66d054c82d4eefad26634368e9
aalonsog <aalonsog@dit.upm.es>
2015-05-20 19:33:56 +0200

    Historical data

--- js/views/nova/instanceDetail/InstanceMonitoringView.js
+++ js/views/nova/instanceDetail/InstanceMonitoringView.js
@@ -99,6 +99,13 @@ var InstanceMonitoringView = Backbone.View.extend({
 
     refresh_stats: function () {
         var self = this;
+        
+        // para dar efecto de que se mueve
+        this.cpu_speed.drawWithInputValue(0);
+        //this.disk_speed.drawWithInputValue(stats[0].percDiskUsed.value);
+        this.disk_speed.drawWithInputValue(0);
+        this.mem_speed.drawWithInputValue(0);
+
         self.model.getMonitoringStats({callback: function(stats){
             if (stats !== undefined) {
                 self.updateSpeedometers(stats);
@@ -110,11 +117,14 @@ var InstanceMonitoringView = Backbone.View.extend({
         if($('#chart_view').hasClass('hide')) {
             $('#chart_view').removeClass('hide');
             $('#speedometer_view').addClass('hide');
+            $('#refresh_button').addClass('hide');
             $('#switch_button').html('Back');
         } else {
             $('#chart_view').addClass('hide');
             $('#speedometer_view').removeClass('hide');
+            $('#refresh_button').removeClass('hide');
             $('#switch_button').html('Graphs');
+            this.refresh_stats();
         }
     },
 
@@ -160,97 +170,107 @@ var InstanceMonitoringView = Backbone.View.extend({
 
     renderCharts: function (scale) {
 
-        console.log('RENDER ', scale, this.historic_data);
+        //console.log('RENDER ', scale, this.historic_data);
 
-        if (this.historic_data) {
+        if (this.historic_data && this.historic_data.length > 0) {
+
+            var labels = [];
+            var cpu_data = [];
+            var mem_data = [];
+            var disk_data = [];
+
+            var last_hour = this.historic_data[this.historic_data.length - 1].timestamp.split('T')[1].split(':')[0];
+            var last_day = this.historic_data[this.historic_data.length - 1].timestamp.split('T')[0].split('-')[2];
+            var last_month = this.historic_data[this.historic_data.length - 1].timestamp.split('T')[0].split('-')[1];
+            var prev_month = parseInt(last_month, 10) - 1;
+
+            switch (scale) {
+                case 'day':
+
+                    for (var h = last_hour - 24; h <= last_hour; h = h + 3) {
+                        if (h < 0) {
+                            labels.push(24 + h + ':00');
+                        } else {
+                            labels.push(h + ':00');
+                        }
+
+                    }
+                    for (var i = this.historic_data.length - 25; i <= this.historic_data.length - 1; i = i + 3) {
+                        if (this.historic_data[i]) {
+                            cpu_data.push(this.historic_data[i].percCPULoad.value);
+                            disk_data.push((this.flavor.get('disk') / 100) * this.historic_data[i].percDiskUsed.value);
+                            mem_data.push((this.flavor.get('ram') / 100) * this.historic_data[i].percRAMUsed.value);
+                        } else {
+                            cpu_data.push(0);
+                            mem_data.push(0);
+                            disk_data.push(0);
+                        }
+                    }
+
+                    break;
+                case 'week':
+    
+                    for (var d = last_day - 7; d <= last_day; d++) {
+                        if (d <= 0) {
+                            labels.push(30 + d + '/' + prev_month);
+                        } else {
+                            labels.push(d + '/' + last_month);
+                        }
+
+                    }
+                    for (var j = this.historic_data.length - 169; j <= this.historic_data.length; j = j + 24) {
+                        if (this.historic_data[j]) {
+                            cpu_data.push(this.historic_data[j].percCPULoad.value);
+                            disk_data.push((this.flavor.get('disk') / 100) * this.historic_data[j].percDiskUsed.value);
+                            mem_data.push((this.flavor.get('ram') / 100) * this.historic_data[j].percRAMUsed.value);
+                        } else {
+                            cpu_data.push(0);
+                            mem_data.push(0);
+                            disk_data.push(0);
+                        }
+                    }
+                    break;
+                case 'month':
+                    for (var m = last_day - 30; m <= last_day; m++) {
+                        if (m <= 0) {
+                            labels.push(30 + m + '/' + prev_month);
+                        } else {
+                            labels.push(m + '/' + last_month);
+                        }
+
+                    }
+                    for (var k = this.historic_data.length - 721; k <= this.historic_data.length; k = k + 24) {
+                        if (this.historic_data[k]) {
+                            cpu_data.push(this.historic_data[k].percCPULoad.value);
+                            disk_data.push((this.flavor.get('disk') / 100) * this.historic_data[k].percDiskUsed.value);
+                            mem_data.push((this.flavor.get('ram') / 100) * this.historic_data[k].percRAMUsed.value);
+                        } else {
+                            cpu_data.push(0);
+                            mem_data.push(0);
+                            disk_data.push(0);
+                        }
+                    }
+
+                    break;
+            }
+
+            this.cpu_dataset.labels = labels;
+            this.cpu_dataset.datasets[0].data = cpu_data;
+
+            this.disk_dataset.labels = labels;
+            this.disk_dataset.datasets[0].data = disk_data;
+
+            this.mem_dataset.labels = labels;
+            this.mem_dataset.datasets[0].data = mem_data;
+            
+            var cpu_ctx = document.getElementById("cpu_chart").getContext("2d");
+            var cpu_chart = new Chart(cpu_ctx).Line(this.cpu_dataset, this.cpu_opt);
+            var disk_ctx = document.getElementById("disk_chart").getContext("2d");
+            var disk_chart = new Chart(disk_ctx).Line(this.disk_dataset, this.disk_opt);
+            var mem_ctx = document.getElementById("mem_chart").getContext("2d");
+            var mem_chart = new Chart(mem_ctx).Line(this.mem_dataset, this.mem_opt);
 
         }
-
-        var labels = [];
-        var cpu_data = [];
-        var mem_data = [];
-        var disk_data = [];
-
-        var now;
-
-        switch (scale) {
-            case 'day':
-
-                now = new Date().getHours();
-
-                for (var h = now - 24; h <= now; h = h + 3) {
-                    if (h < 0) {
-                        labels.push(24 + h + ':00');
-                    } else {
-                        labels.push(h + ':00');
-                    }
-
-                }
-                for (var i = this.historic_data.length - 24; i <= this.historic_data.length; i = i +3) {
-                    if (this.historic_data[i]) {
-                        cpu_data.push(this.historic_data[i].percCPULoad.value);
-                        disk_data.push(Math.round((this.flavor.get('disk') / 100) * this.historic_data[i].percDiskUsed.value));
-                        mem_data.push(Math.round((this.flavor.get('ram') / 100) * this.historic_data[i].percRAMUsed.value));
-                    } else {
-                        cpu_data.push(0);
-                        mem_data.push(0);
-                        disk_data.push(0);
-                    }
-                }
-                break;
-            case 'week':
-                now = new Date().getDay();
-                var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-                for (var d = now - 6; d <= now; d++) {
-                    if (d < 0) {
-                        labels.push(days[days.length + d]);
-                    } else {
-                        labels.push(days[d]);
-                    }
-
-                }
-                // for (var i = this.historic_data.length - 24; i <= this.historic_data.length; i = i +3) {
-                //     if (this.historic_data[i]) {
-                //         cpu_data.push(this.historic_data[i].percCPULoad.value);
-                //         disk_data.push(Math.round((this.flavor.get('disk') / 100) * this.historic_data[i].percDiskUsed.value));
-                //         mem_data.push(Math.round((this.flavor.get('ram') / 100) * this.historic_data[i].percRAMUsed.value));
-                //     } else {
-                //         cpu_data.push(0);
-                //         mem_data.push(0);
-                //         disk_data.push(0);
-                //     }
-                // }
-                break;
-            case 'month':
-                // now = new Date().getDate();
-
-                // for (var d = now - 6; d <= now; d++) {
-                //     if (d < 0) {
-                //         labels.push(days[days.length + d]);
-                //     } else {
-                //         labels.push(days[d]);
-                //     }
-
-                // }
-                break;
-        }
-
-        this.cpu_dataset.labels = labels;
-        this.cpu_dataset.datasets[0].data = cpu_data;
-
-        this.disk_dataset.labels = labels;
-        this.disk_dataset.datasets[0].data = disk_data;
-
-        this.mem_dataset.labels = labels;
-        this.mem_dataset.datasets[0].data = mem_data;
-        
-        var cpu_ctx = document.getElementById("cpu_chart").getContext("2d");
-        var cpu_chart = new Chart(cpu_ctx).Line(this.cpu_dataset, this.cpu_opt);
-        var disk_ctx = document.getElementById("disk_chart").getContext("2d");
-        var disk_chart = new Chart(disk_ctx).Line(this.disk_dataset, this.disk_opt);
-        var mem_ctx = document.getElementById("mem_chart").getContext("2d");
-        var mem_chart = new Chart(mem_ctx).Line(this.mem_dataset, this.mem_opt);
     },
 
     render: function () {
