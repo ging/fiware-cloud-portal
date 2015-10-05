4d20689a336b63c48106076e6be135f54f612173
naruhodo-ryuichi <pm.verdugo@alumnos.upm.es>
2015-10-05 20:23:23 +0200

    chef cookbook update

--- extras/chef/fiware-cloud-portal/attributes/default.rb
+++ extras/chef/fiware-cloud-portal/attributes/default.rb
@@ -2,6 +2,6 @@
 # attributes to be applied to nodes that run this cookbook.
 
 # Set a default name
-default['fiware-cloud-portal'][:app_dir] = "/opt/fiware-cloud-portal"
+default['fiware-cloud-portal'][:app_dir] = '/opt/fiware-cloud-portal'
 
 # For further information, see the Chef documentation (http://docs.getchef.com/essentials_cookbook_attribute_files.html).
