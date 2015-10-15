38e4e0ce1dd48e7f8c99c27f29e5efc39311280e
naruhodo-ryuichi <pm.verdugo@alumnos.upm.es>
2015-10-15 16:43:53 +0200

    chef cookbook update

--- extras/chef/fiware-cloud-portal/recipes/default.rb
+++ extras/chef/fiware-cloud-portal/recipes/default.rb
@@ -9,13 +9,13 @@ end
 
 bash :get_system do
   code <<-EOH
-    sudo cd /opt && \
+    cd /opt && \
     sudo git clone https://github.com/ging/fiware-cloud-portal.git && \
     sudo cd #{node['fiware-cloud-portal'][:app_dir]} && \
     sudo mkdir $HOME/.npm && \
     sudo chown -R $(whoami) "$HOME/.npm" && \
     sudo npm install && \
     sudo ./node_modules/grunt-cli/bin/grunt && \
-    sudo cp config.js.template config.js
+    cp config.js.template config.js
   EOH
 end
