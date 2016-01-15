3547d3a2d9e3d2cb1b693eea95c53b727518598b
aalonsog <aalonsog@dit.upm.es>
2016-01-15 12:22:34 +0100

    Warning when trying to access not available regions

--- js/views/SideBarView.js
+++ js/views/SideBarView.js
@@ -6,6 +6,13 @@ var SideBarView = Backbone.View.extend({
         this.model.bind('change:actives', this.render, this);
         this.options.loginModel = UTILS.GlobalModels.get("loginModel");
         this.options.loginModel.bind('change:tenant_id', this.render, this);
+      
+        $('#accept_region_modal_button').on('click', function (e) {
+            $('#no_sanity_modal').unbind('hidden');
+            $('#no_sanity_modal').modal('hide');
+            console.log("Switching to: ", val);
+            window.location = $('.chosen-select').chosen().val();
+        });
     },
 
     render: function (title, showTenants) {
@@ -18,11 +25,26 @@ var SideBarView = Backbone.View.extend({
                 window.location = val;
             }
         });
+
+
                
         $('.chosen-select' ).chosen().change(function(){
             val = $('.chosen-select').chosen().val();
-            console.log("region: ", val);
-            window.location = val;
+            
+            var region = $('.chosen-select').find(":selected").text();
+            var status = UTILS.Auth.getRegionStatus(region);
+
+            if (status === 'down') {
+                $('#no_sanity_modal').on('hidden.bs.modal', function (e) {
+                    $('#no_sanity_modal').unbind('hidden');
+                    console.log("Returning to: ", UTILS.Auth.getCurrentRegion());
+                    window.location.href = '#reg/switch/' + UTILS.Auth.getCurrentRegion() + '/';
+                });
+                $('#no_sanity_modal').modal();
+            } else {
+                console.log("Switching to: ", val);
+                window.location = val;
+            }
         });
 
         $('.btn-openrc').on('click', this.openModal);
